e77a0db0ba5ce37c62d3665a993eaa104dc63e8a
breannasanchez <breannasanchez@computingforall.org>
2021-08-12 14:21:48 -0700

    edited javascript file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+// var, let, or cont indicates a delaration
+// when you see "-" it indicates an assignment
+var firstname = "Breanna";
+let lastname = "Sanchez";
+//You cannot reassign constant variables!
+const age = 17;
+//variable reassignments
+firstName = "My Dude";
+lastName = "Potates";
+
+console.log('Hello' + ' ' + firstName + ' ' + lastName + '!' + ' You are ' + age + ' years old!');
